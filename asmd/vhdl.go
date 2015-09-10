9955c560b2fca9d541f5483fc78144a259296e58
Jon Kunkee <jonathan.kunkee@gmail.com>
2015-09-09 23:48:12 -0700

    State traversal and write-out is done. Still need VHDL value literals, transition subnet cycle detection, and corrected state_reg printouts (sensitivity list, port).

--- asmd/vhdl.go
+++ asmd/vhdl.go
@@ -153,7 +153,7 @@ func (m *StateMachine) VHDL(filename string) (err error) {
 		} else {
 			write(file, ", ")
 		}
-		write(file, stateName+"_s")
+		write(file, stateName)
 	}
 	write(file, "\n")
 	// State machine signals
@@ -163,26 +163,128 @@ func (m *StateMachine) VHDL(filename string) (err error) {
 	write(file, "begin\n")
 
 	// Register process
-	write(file, m.indent(1), "-- FSM state register\n")
-	write(file, m.indent(1), "process(clk, rst)\n")
+	write(file, m.indent(1), "-- Register Process\n")
+	write(file, m.indent(1), "process(clk")
+	if *m.Options.AddAsyncReset {
+		write(file, ", rst")
+	}
+	write(file, ")\n")
 	write(file, m.indent(1), "begin\n")
-	write(file, m.indent(2), "if (rst='1') then\n")
-	write(file, m.indent(3), "state_reg <= ", m.Options.FirstState, ";\n")
+	if *m.Options.AddAsyncReset {
+		write(file, m.indent(2), "if (rst='1') then\n")
+		write(file, m.indent(3), "-- async reset of registers\n")
+		write(file, m.indent(3), "state_reg <= ", m.Options.FirstState, ";\n")
+		for name, reg := range m.Registers {
+			defVal := reg.DefaultValue
+			if defVal == "0" {
+				defVal = "(others => '0')"
+			}
+			write(file, m.indent(3), name+"_reg", " <= ", defVal, ";\n")
+		}
+	}
+	write(file, m.indent(2))
+	if *m.Options.AddAsyncReset {
+		write(file, "els")
+	}
 	if m.Options.ClockType == "posedge" {
-		write(file, m.indent(2), "elsif (clk'event and clk='1') then\n")
+		write(file, "if (clk'event and clk='1') then\n")
 	} else if m.Options.ClockType == "negedge" {
-		write(file, m.indent(2), "elsif (clk'event and clk='0') then\n")
+		write(file, "if (clk'event and clk='0') then\n")
 	} else {
 		return errors.New("Unrecognized clock type: " + m.Options.ClockType)
 	}
+	write(file, m.indent(3), "-- FSM state register\n")
 	write(file, m.indent(3), "state_reg <= state_next;\n")
+	write(file, m.indent(3), "-- algorithm registers\n")
+	for name, _ := range m.Registers {
+		write(file, m.indent(3), name+"_reg", " <= ", name+"_next", ";\n")
+	}
 	write(file, m.indent(2), "end if;\n")
 	write(file, m.indent(1), "end process;\n")
-	// Next State process
-	// Mealy(?) Output process
+	write(file, "\n")
+
+	// Next State + Output + RTL Operation process
+	write(file, m.indent(1), "-- Next State + Output + RTL Operation process\n")
+	write(file, m.indent(1), "process(")
+	// TODO add in proper sensitivity list inference instead of this "benign sledgehammer" approach
+	isFirst = true
+	for name, _ := range m.Inputs {
+		if name == "clk" || name == "rst" {
+			continue
+		}
+		if !isFirst {
+			write(file, ", ")
+		} else {
+			isFirst = false
+		}
+		write(file, name)
+	}
+	for name, _ := range m.Registers {
+		if !isFirst {
+			write(file, ", ")
+		} else {
+			isFirst = false
+		}
+		write(file, name+"_reg", ", ", name+"_next")
+	}
+	write(file, ")\n")
+	write(file, m.indent(1), "begin\n")
+	// default register 'next's to previous value
+	write(file, m.indent(2), "state_next <= state_reg;\n")
+	for regName, _ := range m.Registers {
+		write(file, m.indent(2), regName+"_next", " <= ", regName+"_reg", ";\n")
+	}
+	for outName, out := range m.Outputs {
+		write(file, m.indent(2), outName, " <= ", out.DefaultValue, ";\n")
+	}
+	// state switch statement
+	write(file, m.indent(2), "case state_reg is\n")
+	for stateName, state := range m.States {
+		if state.IsMealy {
+			continue
+		}
+		write(file, m.indent(3), "case ", stateName, " =>\n")
+		writeVhdlNextNetwork(file, 4, m, state.Next)
+		for regName, operation := range state.Operations {
+			write(file, m.indent(4), regName+"_next", " <= ", operation, ";\n")
+		}
+	}
+	write(file, m.indent(2), "end case;\n")
+	write(file, m.indent(1), "end process;\n")
+
+	// Functional units
+	for unitName, unit := range m.FunctionalUnits {
+		write(file, "\n")
+		write(file, m.indent(1), "-- FunctionalUnit ", unitName, " with ", strconv.Itoa(len(unit.Registers)), " registers\n")
+	}
+
 	// architecture end
 	write(file, "end Behavioral;\n")
 	write(file, "\n")
 
 	return nil
+}
+
+func writeVhdlNextNetwork(file *os.File, indentLevel uint, m *StateMachine, nextThingName string) {
+	if nextState, ok := m.States[nextThingName]; ok && !nextState.IsMealy {
+		// base case
+		// TODO validate this is valid during Parse
+		write(file, m.indent(indentLevel), "state_reg <= ", nextThingName, ";\n")
+	} else if nextState, ok := m.States[nextThingName]; ok && nextState.IsMealy {
+		for out, action := range nextState.Operations {
+			varName := out
+			if _, ok := m.Registers[out]; ok {
+				varName = out + "_next"
+			}
+			write(file, m.indent(indentLevel), varName, " <= ", action, ";\n")
+		}
+		writeVhdlNextNetwork(file, indentLevel, m, nextState.Next)
+	} else if cond, ok := m.Conditions[nextThingName]; ok {
+		// TODO support elsif
+		write(file, m.indent(indentLevel), "if ", cond.Expression, " then\n")
+		writeVhdlNextNetwork(file, indentLevel+1, m, cond.TrueTarget)
+		write(file, m.indent(indentLevel), "else\n")
+		writeVhdlNextNetwork(file, indentLevel+1, m, cond.FalseTarget)
+		write(file, m.indent(indentLevel), "end if;\n")
+	}
 }
